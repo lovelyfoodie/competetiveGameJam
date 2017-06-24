bf34ac788bb9a829b15dc5308e12eb9489816ac8
lovelyfoodie <bonnieray12@gmail.com>
2017-06-24 16:24:58 -0400

    left player controller

---
+++ Assets/LeftPlayerController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NUnit.Framework;
+
+public class LeftPlayerController : MonoBehaviour {
+
+	public GameObject item;
+
+	void Update() {
+		if (Input.GetKeyDown (KeyCode.A)) { //left key press
+			Instantiate (item, gameObject.transform.position, gameObject.transform.rotation);
+		}
+			
+
+	}
+}
