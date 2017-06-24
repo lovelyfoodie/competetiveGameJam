e39c0638d5ec1b04c04f0020f051944d3148a0b8
lovelyfoodie <bonnieray12@gmail.com>
2017-06-24 18:09:42 -0400

    throwable spawner on left playah

--- Assets/LeftPlayerController.cs
+++ Assets/LeftPlayerController.cs
@@ -3,12 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 using NUnit.Framework;
 
+[RequireComponent(typeof(ThrowableSpawner))]
+
 public class LeftPlayerController : MonoBehaviour {
 
-	public GameObject item;
-
+	private GameObject item;
+	private ThrowableSpawner spawner;
 	private const float ACCELERATION = 210.172341f; 
 
+	void Start() {
+
+		spawner = GetComponent<ThrowableSpawner> ();
+	}
 
 	void Update() {
 
@@ -16,6 +22,7 @@ public class LeftPlayerController : MonoBehaviour {
 		Rigidbody2D thrownRigidBody;
 
 		if (Input.GetKeyDown (KeyCode.A)) { //left key press
+			item = spawner.GetThrowable ();
 			thrownItem = Instantiate (item, gameObject.transform.position, gameObject.transform.rotation) as GameObject;
 			thrownRigidBody = thrownItem.GetComponent <Rigidbody2D>();
 			float objMass = thrownRigidBody.mass;
