5ddb1869b468ebeb415822875d74d53c1c2a029f
EnkiduRed <rbrolley@alchemedium.com>
2017-06-24 16:08:25 -0400

    throwable GO added

---
+++ Assets/Scripts/Throwable.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Collider2D))]
+[RequireComponent(typeof(Rigidbody2D))]
+public class Throwable : MonoBehaviour {
+
+    public ThrowableData data;
+    
+    public float Area
+    {
+        get
+        {
+            if (Collider is CapsuleCollider2D)
+            {
+                CapsuleCollider2D c = (Collider as CapsuleCollider2D);
+                return c.size.x * c.size.y;
+            } 
+            else
+            {
+                throw new System.Exception("Collider type not defined!");
+            }
+        }
+    }
+    public float Mass
+    {
+        get
+        {
+            return data.density * Area;
+        }
+    }
+
+    private Collider2D _collider = null;
+    public Collider2D Collider
+    {
+        get
+        {
+            if (_collider == null)
+                _collider = GetComponent<Collider2D>();
+
+            return _collider;
+        }
+    }
+    private Rigidbody2D _rb = null;
+    public Rigidbody2D Rigidbody
+    {
+        get
+        {
+            if (_rb == null)
+                _rb = GetComponent<Rigidbody2D>();
+
+            return _rb;
+        }
+    }
+
+    private void Awake()
+    {
+        Rigidbody.mass = Mass;
+    }
+    private void OnValidate()
+    {
+        Rigidbody.mass = Mass;
+    }
+
+}
---
+++ Assets/Scripts/ThrowableData.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "ThrowableData", menuName = "Throwables", order = 1)]
+public class ThrowableData : ScriptableObject
+{
+    public float density;
+}
