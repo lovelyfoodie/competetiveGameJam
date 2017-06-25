4bbf7747bf84bb33f5117df55034c57d98d0d5df
pkohler <pkohler48@gmail.com>
2017-06-24 20:32:15 -0400

    Prototype Push
    Right Player added

---
+++ Assets/Scripts/RightPlayerController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NUnit.Framework;
+using System.Security.Cryptography;
+
+[RequireComponent(typeof(ThrowableSpawner))]
+
+public class RightPlayerController : MonoBehaviour {
+
+	private GameObject item;
+	private ThrowableSpawner spawner;
+	private const float ACCELERATION = 210.172341f; 
+	private Tower tower; 
+
+	void Start() {
+
+		spawner = GetComponent<ThrowableSpawner> ();
+		tower = FindObjectOfType<Tower> ();
+	}
+
+	void Update() {
+
+		GameObject thrownItem;
+		Rigidbody2D thrownRigidBody;
+
+		if (Input.GetKeyDown (KeyCode.D)) { //left key press
+			item = spawner.GetThrowable ();
+			thrownItem = Instantiate (item, gameObject.transform.position, gameObject.transform.rotation) as GameObject;
+			thrownRigidBody = thrownItem.GetComponent <Rigidbody2D>();
+			float objMass = thrownRigidBody.mass;
+			float thrust = objMass * ACCELERATION; 
+			thrownRigidBody.AddForce (transform.up * thrust);
+			float angleOfRelease = transform.rotation.z;
+			float xForce = Mathf.Cos (angleOfRelease) * thrust * -.1f; 
+			tower.addForce (xForce);
+		}
+
+
+			
+
+	}
+}
