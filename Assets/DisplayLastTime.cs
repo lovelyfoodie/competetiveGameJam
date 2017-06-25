b6b37aeb2b0ed8c826a885de724943ede60ec781
pkohler <pkohler48@gmail.com>
2017-06-25 00:13:17 -0400

    Added Previous Score To Screen UI

---
+++ Assets/DisplayLastTime.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DisplayLastTime : MonoBehaviour {
+
+	Text displayText;
+
+	// Use this for initialization
+	void Start () {
+		displayText = GetComponent<Text> ();
+		string text = PlayerPrefs.GetString ("FinalScore");
+		if (text != null) {
+			displayText.text = text;
+		} else {
+			displayText.text = "None";
+		}
+	}
+
+}
