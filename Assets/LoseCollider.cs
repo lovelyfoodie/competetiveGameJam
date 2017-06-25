ec40296532b6e031c8518da9c62c43ba7cc5d2fa
lovelyfoodie <bonnieray12@gmail.com>
2017-06-24 23:29:40 -0400

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	Assets/Fonts.meta
    #	Assets/StreamingAssets/Audio/GeneratedSoundBanks/Mac.meta
    #	Assets/StreamingAssets/Audio/GeneratedSoundBanks/Mac/SoundBanks.meta
    #
    Assets/StreamingAssets/Audio/GeneratedSoundBanks/Mac/SoundBanks/Default
    Work Unit.wwu.meta
    #	Assets/StreamingAssets/Audio/GeneratedSoundBanks/temp.txt.meta
    #
    Assets/Wwise/Deployment/Plugins/Windows/x86/Profile/AkSoundEngine.pdb.me
    ta
    #	Assets/Wwise/Editor/ProjectData/AkWwiseProjectData.asset

---
+++ Assets/LoseCollider.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoseCollider : MonoBehaviour {
+
+	private TimeDisplay timer;
+	// Use this for initialization
+	void Start () {
+		timer = FindObjectOfType<TimeDisplay> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		Debug.Log (other.transform.name);
+		if (other.transform.name.Equals ("PlayerPlatform")) {
+			timer.Stop ();
+		}
+	}
+}
---
+++ Assets/TimeDisplay.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class TimeDisplay : MonoBehaviour {
+
+	private Text timeDisplay;
+	private float time;
+	private string minutes;
+	private string seconds;
+	private bool runTimer;
+	// Use this for initialization
+	void Awake () {
+		time = 0f;
+		runTimer = true;
+		timeDisplay = GetComponent<Text> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (runTimer) {
+			time = time + Time.deltaTime;
+			minutes = Mathf.Floor (time / 60).ToString ("#0");
+			seconds = Mathf.Floor (time % 60).ToString ("00");
+			timeDisplay.text = minutes + ":" + seconds;
+		}
+	}
+
+	public void Stop (){
+		runTimer = false;
+	}
+}
