b678f131171ac9b77566d478a9a8ac5f052139d4
EnkiduRed <rbrolley@alchemedium.com>
2017-06-29 20:39:34 -0400

    keyboard navigation added

---
+++ Assets/InputListener.cs
@@ -0,0 +1,16 @@
+﻿
+using UnityEngine;
+using UnityEngine.Events;
+
+public class InputListener : MonoBehaviour {
+    public KeyCode key;
+
+    public UnityEvent OnInputEvent;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(key))
+            OnInputEvent.Invoke();
+    }
+
+}
