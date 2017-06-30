f1694a84d97c9a20dcb3ebc0cd6f9c46406c5e42
EnkiduRed <rbrolley@alchemedium.com>
2017-06-29 23:51:15 -0400

    added variation in throwing trajectory

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -18,27 +18,26 @@ public class PlayerController : MonoBehaviour {
     public Transform throwPointLeft;
     public Transform throwPointRight;
     public Transform holdPoint;
-    public Vector3 throwAngle = new Vector3(0, 0, 72f);
-    public GameObject held;
-    //[Tooltip("1f = left, -1f = right")]
-    //public float direction = 1f;
+    public Vector3 minThrowAngle = new Vector3(-1f, 1f, 0f);
+    public Vector3 maxThrowAngle = new Vector3(-1f, 1f, 0f);
     public SpriteRenderer sprite;
-
-	private GameObject item;
-	private ThrowableSpawner spawner;
+    
+	private ThrowableSpawner _spawner;
 	private TowerControl _tower;
+
+    private GameObject _held;
 
     void Awake()
     {
-        //if(throwPoint == null)
-        //    throwPoint = GetComponent<Transform>();
-
         if (holdPoint == null)
             holdPoint = GetComponent<Transform>();
     }
 	void Start() {
-		spawner = GetComponent<ThrowableSpawner> ();
+		_spawner = GetComponent<ThrowableSpawner> ();
 		_tower = GetComponentInParent<TowerControl> ();
+
+        minThrowAngle.Normalize();
+        maxThrowAngle.Normalize();
 
         // Init held throwable.
         LoadNextItem();
@@ -46,12 +45,8 @@ public class PlayerController : MonoBehaviour {
 
     void Update() {
 
-		GameObject thrownItem;
-		Rigidbody2D thrownRigidBody;
-
         bool left = Input.GetKeyDown(fireLeft);
         bool right = Input.GetKeyDown(fireRight);
-
 
         if (left || right) {
             float directionMod = left ? 1f : -1f;
@@ -60,46 +55,40 @@ public class PlayerController : MonoBehaviour {
             sprite.flipX = right;
 
             // Get next throwable.
-            thrownItem = held;
+            var thrownItem = _held;
             thrownItem.transform.SetParent(null);
             thrownItem.transform.position = left ? throwPointLeft.transform.position : throwPointRight.transform.position;
             thrownItem.transform.rotation = gameObject.transform.rotation;
-
             thrownItem.GetComponent<Collider2D>().enabled = true;
 
-            thrownRigidBody = thrownItem.GetComponent <Rigidbody2D>();
+            var thrownRigidBody = thrownItem.GetComponent <Rigidbody2D>();
             thrownRigidBody.isKinematic = false;
 
             // Physics.
             float objMass = thrownRigidBody.mass;
-			float thrust = objMass * ACCELERATION; 
-			//thrownRigidBody.AddForce (throwAngle * thrust * directionMod); //good
-            thrownRigidBody.AddForce(transform.up * thrust * directionMod);
+			float thrust = objMass * ACCELERATION;
+            Vector2 angle = Vector2.Lerp(minThrowAngle, maxThrowAngle, Random.value);
+			thrownRigidBody.AddForce(new Vector2(angle.x * thrust * directionMod, angle.y * thrust)); 
             thrownRigidBody.AddTorque(Random.Range(-1f * throwTorque * thrust, throwTorque * thrust));
+            _tower.AddXForce(directionMod * thrust * .1f);
 
-   //         float angleOfRelease = throwAngle.z * directionMod;
-   //         //float angleOfRelease = Quaternion.Euler(throwAngle).z * directionMod;
-   //         //float angleOfRelease = transform.rotation.z * directionMod;
-   //         float xForce = Mathf.Cos (angleOfRelease) * thrust * .1f; 
-			//tower.addForce (xForce);
-
-            _tower.addForce(directionMod * thrust * .1f);
-
+            // Process throw.
             thrownItem.GetComponent<Throwable>().Throw();
 
+            // Queue next items.
             LoadNextItem();
         }
 	}
 
     void LoadNextItem()
     {
-        held = spawner.GetThrowable();
-        held.layer = isTower1 ? 8 : 13;
-        held.transform.SetParent(holdPoint);
-        held.transform.localPosition = Vector3.zero;
-        held.transform.localRotation = Quaternion.identity;
-        held.GetComponent<Rigidbody2D>().isKinematic = true;
-        held.GetComponent<Collider2D>().enabled = false;
+        _held = _spawner.GetThrowable();
+        _held.layer = isTower1 ? 8 : 13;
+        _held.transform.SetParent(holdPoint);
+        _held.transform.localPosition = Vector3.zero;
+        _held.transform.localRotation = Quaternion.identity;
+        _held.GetComponent<Rigidbody2D>().isKinematic = true;
+        _held.GetComponent<Collider2D>().enabled = false;
     }
 }
 
